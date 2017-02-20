d0224a8c38f3930bc716946463c653d186ab81d1
Bjorn Tipling <bjorn@ambientchill.com>
2017-02-20 13:24:18 -0800

    Running cycle.js demo app with es6 modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import header from './header';
-
+import xs from 'xstream';
+import {run} from '@cycle/xstream-run';
+import {makeDOMDriver, h1} from '@cycle/dom';
 
 function main() {
-  console.log('wtf?');
-  header();
+  const sinks = {
+    DOM: xs.periodic(1000).map(i =>
+      h1('' + i + ' seconds elapsed')
+    )
+  };
+  return sinks;
 }
-main();
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
