f5d9c2f62c4747e8b88515d936163d02a596fce5
Bjorn Tipling <bjorn@ambientchill.com>
2017-02-20 22:56:11 -0800

    Figured out events

--- src/connect.ts
+++ src/connect.ts
@@ -1,11 +1,31 @@
-import { div } from '@cycle/dom';
+import {div, input, VNode, DOMSource} from '@cycle/dom';
 import { ISinks } from './interfaces/sinks';
-import xs  from 'xstream';
+import { Stream } from 'xstream';
+
+
+function intent (DOM: DOMSource): Stream<string> {
+  return DOM.select('.path').events('keyup')
+    .map(ev => {
+      return (<HTMLInputElement> ev.target).value;
+    })
+    .startWith('Nothing')
+    .debug(msg => console.log(`msg ${msg}`));
+}
+
+function view (value$: Stream<string>): Stream<VNode> {
+  return value$.map(value =>
+    div([
+      'Connect UI',
+      input('.path'),
+      div(`value: '${value}'`),
+    ]),
+  );
+}
 
 export default function Connect (sources): ISinks {
+  const change$ = intent(sources.DOM);
+  const vtree$ = view(change$);
   return {
-    DOM: xs.of(
-      div('Connect UI'),
-    ),
+    DOM: vtree$,
   };
 }
