3bb6782d191de15241b57bb5073e2fd6c1818af3
Bjorn Tipling <bjorn@ambientchill.com>
2017-02-21 09:20:37 -0800

    Connect input with button

--- src/connect.ts
+++ src/connect.ts
@@ -1,22 +1,28 @@
-import {div, input, VNode, DOMSource} from '@cycle/dom';
+import {div, input, VNode, DOMSource, button} from '@cycle/dom';
 import { ISinks } from './interfaces/sinks';
-import { Stream } from 'xstream';
+import xs, { Stream } from 'xstream';
 
 
 function intent (DOM: DOMSource): Stream<string> {
-  return DOM.select('.path').events('keyup')
-    .map(ev => {
-      return (<HTMLInputElement> ev.target).value;
-    })
-    .startWith('Nothing')
-    .debug(msg => console.log(`msg ${msg}`));
+
+  const input$ = DOM.select('.connect-path').events('keyup')
+  .map((ev) => {
+    return (<HTMLInputElement> (<Event> ev).target).value;
+  });
+
+  const submitted$ = DOM.select('.connect-submit').events('click');
+
+  return input$.map(inputVal => submitted$.map(() => inputVal)).flatten()
+  .startWith('Nothing')
+  .debug(msg => console.log(`msg ${msg}`));
 }
 
 function view (value$: Stream<string>): Stream<VNode> {
   return value$.map(value =>
     div([
-      'Connect UI',
-      input('.path'),
+      'Connect path',
+      input('.connect-path'),
+      button('.connect-submit', 'connect'),
       div(`value: '${value}'`),
     ]),
   );
