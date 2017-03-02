4ae4843cc9f901a9e887e8bdf904a96e74cff60f
Bjorn Tipling <bjorn@ambientchill.com>
2017-03-01 22:37:19 -0800

    show multiple messages from stdout at once

--- src/components/info.ts
+++ src/components/info.ts
@@ -1,13 +1,19 @@
-import {h2, pre, VNode} from '@cycle/dom';
+import { pre, VNode} from '@cycle/dom';
+import { compose, concat, split, subtract, takeLast } from 'ramda';
 import { Stream } from 'xstream';
 import { IInfoSinks } from '../interfaces/sinks';
-import {ISources} from '../interfaces/sources';
+import { ISources } from '../interfaces/sources';
 
 function view(run$: Stream<string>): Stream<VNode> {
+  const lineSplitter = split('\n');
+  const maxLines = takeLast(100);
+  const messagesToDisplay = compose(maxLines, concat);
+
   return run$
-    .startWith('Input will appear here.')
-    .map((value) =>
-    pre(value),
+    .startWith('Log messages will appear here.')
+    .map(lineSplitter)
+    .fold(messagesToDisplay, [])
+    .map((value) => pre(value.join('\n')),
   );
 }
 
