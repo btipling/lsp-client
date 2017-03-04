ca72b82e427f6f156e2be913a22b146d1f394da0
Bjorn Tipling <bjorn@ambientchill.com>
2017-03-04 14:44:25 -0800

    more cleanup

--- src/components/connect.ts
+++ src/components/connect.ts
@@ -1,13 +1,15 @@
 import { button, div, DOMSource, form, input, VNode } from '@cycle/dom';
-import xs, { Stream } from 'xstream';
+import { Stream } from 'xstream';
 import { IConnectSinks } from '../interfaces/sinks';
 
 export type ConnectStream = { value: string, submitEvent: Event };
 
 function intent(DOM: DOMSource): Stream<ConnectStream> {
   // Gets input value and submit event from DOM.
+
   const eventToValue = ev => ((ev as Event).target as HTMLInputElement).value;
-  const input$ = DOM.select('.connect-path').events('keyup')
+
+  const input$ = DOM.select('.connect-path').events('keyup');
   const submitted$ = DOM.select('.connect-form').events('submit');
 
   return input$
@@ -18,6 +20,7 @@ function intent(DOM: DOMSource): Stream<ConnectStream> {
 
 function view(connect$: Stream<ConnectStream>): Stream<VNode> {
   // Creates connection input form.
+
   return connect$.map(({ value }) =>
     form('.connect-form', [
       'Connect path',
